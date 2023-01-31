9fedc0f7a58fde2104b1bac17ce8b33e5b0b3567
Osaigbovo idiaghe <idiagheosaigbovoosas@gmail.com>
2023-01-31 15:25:29 +0100

    React form using React-Hook

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+export const Form = () => {
+  return (
+    <form>
+      <input type="text" placeholder="Full Name.." />
+      <input type="text" placeholder="Email.." />
+      <input type="numb" placeholder="Age.." />
+      <input type="password" placeholder="Password..." />
+      <input type="password" placeholder="Confirm Password.." />
+      <input type="submit" />
+    </form>
+  );
+};
+
+export default Form;
